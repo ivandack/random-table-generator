6be9828c80bf3d85472b6bd43684563d2ab16cea
Ivan Dackiewicz <ivandack@gmail.com>
2024-02-13 23:08:33 -0300

    Updated router mechanism

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
 import { ThemeProvider } from "@mui/material/styles";
-import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from "@mui/material/CssBaseline";
 
 import App from "./App.tsx";
 import { MuiTheme } from "./theme/muiTheme.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: (
       <ThemeProvider theme={MuiTheme}>
         <CssBaseline />
         <App />
       </ThemeProvider>
-    </BrowserRouter>
+    ),
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
